091c406cac64de6efe9b0afbe9a920de6c13dd37
Misu099 <mihaialexvp@gmail.com>
2024-05-18 23:58:42 +0300

    made it with a lot of help from gpt, to revise it later and do it alone

--- src/InteractiveStudentGrades.java
+++ src/InteractiveStudentGrades.java
@@ -107,7 +107,6 @@ public class InteractiveStudentGrades {
         }
     }
 
-
     private static void displayAllGrades(HashMap<Integer, String> studentGrades) {
         System.out.println(ANSI_BLUE + "\nAll Student Grades:" + ANSI_RESET);
         for (Map.Entry<Integer, String> entry : studentGrades.entrySet()) {
