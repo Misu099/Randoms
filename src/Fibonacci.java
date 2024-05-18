82ecdae970bd1e8effb5b320d5038f9399cbe0ec
Misu099 <mihaialexvp@gmail.com>
2024-05-18 23:56:01 +0300

    should be ok

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,21 +1,19 @@
 public class Fibonacci {
 
 
-
-
-    public int fib(int n){
-        if (n == 0 || n == 1){
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
             return n;
         }
-        return (fib(n-2)+fib(n-1));
+        return (fib(n - 2) + fib(n - 1));
     }
-
-    public int[] fibArr(int n){
+//TODO:recursion might not be the most efficient, to see later
+    public int[] fibArr(int n) {
         int[] arr = new int[n];
         arr[0] = 0;
         arr[1] = 1;
-        for (int i = 2; i < n; i++){
-            arr[i] = arr[i-2] + arr[i-1];
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
         }
         return arr;
     }
